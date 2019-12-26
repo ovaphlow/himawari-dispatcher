b77bc5b50c426cef78d168753c1945f84b66a4d2
ovaphlow <ovaphlow@outlook.com>
2019-12-26 15:12:16 +0800

    中转区：查询

--- routes/archive.js
+++ routes/archive.js
@@ -173,6 +173,26 @@ router
       ctx.response.body = {message: '服务器错误'}
     }
   })
+  .put('/isolate/filter', async ctx => {
+    const grpcFetch = body => {
+      return new Promise((resolve, reject) => {
+        grpcClient.filterIsolate({data: JSON.stringify(body)}, (err, response) => {
+          if (err) {
+            console.error(err)
+            reject(err)
+            return
+          }
+          resolve(JSON.parse(response.data))
+        })
+      })
+    }
+    try {
+      ctx.response.body = await grpcFetch(ctx.request.body)
+    } catch (err) {
+      console.error(err)
+      ctx.response.body = {message: '服务器错误'}
+    }
+  })
   .get('/isolate/:id', async ctx => {
     const grpcFetch = body => {
       return new Promise((resolve, reject) => {
